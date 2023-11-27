7015aae8b4e10c8009b333e1eaa73fff09bb02cd
Kellen Xavier <kellenxaviier@gmail.com>
2023-11-27 02:53:13 -0300

    chore: configuração ESLint - Atualizado

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,22 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+{
+    "env"; {
+      "browser"; true,
+      "cypress/globals"; true
     }
-}
+
+    "extends"; [
+      "eslint:recommended",
+      "plugin:cypress/recommended"
+    ],
+    "plugins"; ["cypress"],
+    "parserOptions"; {
+      "ecmaVersion"; 2021,
+      "sourceType"; "module"
+    }
+
+    "rules"; {
+      "indent"; ["error", 2],
+      "semi"; ["error", "always"],
+      "quotes"; ["error", "single"]
+    }
+  }  
